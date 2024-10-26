625802d06b9ecd0bfc4972b95d0b4fb34097e537
Vivek Tripathi <75514312+vivektrip18@users.noreply.github.com>
2024-10-26 12:12:21 +0530

    Update Blogs.tsx

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,38 +1,52 @@
-import { Appbar } from "../components/Appbar"
-import { BlogCard } from "../components/BlogCard"
+import { Appbar } from "../components/Appbar";
+import { BlogCard } from "../components/BlogCard";
 import { BlogSkeleton } from "../components/BlogSkeleton";
 import { useBlogs } from "../hooks";
 
 export const Blogs = () => {
     const { loading, blogs } = useBlogs();
     
+    // Get the current date formatted as "2nd Feb 2024"
+    const currentDate = new Date().toLocaleDateString('en-US', {
+        day: 'numeric',
+        month: 'short',
+        year: 'numeric',
+    });
+
     if (loading || !blogs) {
-        return <div>
-            <Appbar /> 
-            <div  className="flex justify-center">
+        return (
+            <div>
+                <Appbar /> 
+                <div className="flex justify-center">
+                    <div>
+                        <BlogSkeleton />
+                        <BlogSkeleton />
+                        <BlogSkeleton />
+                        <BlogSkeleton />
+                        <BlogSkeleton />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <Appbar />
+            <div className="flex justify-center">
                 <div>
-                    <BlogSkeleton />
-                    <BlogSkeleton />
-                    <BlogSkeleton />
-                    <BlogSkeleton />
-                    <BlogSkeleton />
+                    {blogs.map(blog => (
+                        <BlogCard
+                            key={blog.id}
+                            id={blog.id}
+                            authorName={blog.author.name || "Anonymous"}
+                            title={blog.title}
+                            content={blog.content}
+                            publishedDate={currentDate} // Use current date here
+                        />
+                    ))}
                 </div>
             </div>
         </div>
-    }
-
-    return <div>
-        <Appbar />
-        <div  className="flex justify-center">
-            <div>
-                {blogs.map(blog => <BlogCard
-                    id={blog.id}
-                    authorName={blog.author.name || "Anonymous"}
-                    title={blog.title}
-                    content={blog.content}
-                    publishedDate={"2nd Feb 2024"}
-                />)}
-            </div>
-        </div>
-    </div>
-}
+    );
+};

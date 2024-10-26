1ee9bd35cd41787435fc5ad114374c82ab8fb857
Vivek Tripathi <75514312+vivektrip18@users.noreply.github.com>
2024-10-26 13:53:51 +0530

    Update Blogs.tsx

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,45 +1,40 @@
-import { Appbar } from "../components/Appbar";
-import { BlogCard } from "../components/BlogCard";
+
+import { Appbar } from "../components/Appbar"
+import { BlogCard } from "../components/BlogCard"
 import { BlogSkeleton } from "../components/BlogSkeleton";
 import { useBlogs } from "../hooks";
 
 export const Blogs = () => {
     const { loading, blogs } = useBlogs();
-
+    
     if (loading || !blogs) {
-        return (
-            <div>
-                <Appbar />
-                <div className="flex justify-center">
-                    <div>
-                        <BlogSkeleton />
-                        <BlogSkeleton />
-                        <BlogSkeleton />
-                        <BlogSkeleton />
-                        <BlogSkeleton />
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
-    return (
-        <div>
-            <Appbar />
-            <div className="flex justify-center">
+        return <div>
+            <Appbar /> 
+            <div  className="flex justify-center">
                 <div>
-                    {blogs.map(blog => (
-                        <BlogCard
-                            key={blog.id}
-                            id={blog.id}
-                            authorName={blog.author.name || "Anonymous"}
-                            title={blog.title}
-                            content={blog.content}
-                            publishedDate={blog.publishedDate} // Pass the date directly
-                        />
-                    ))}
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
                 </div>
             </div>
         </div>
-    );
-};
+    }
+
+    return <div>
+        <Appbar />
+        <div  className="flex justify-center">
+            <div>
+                {blogs.map(blog => <BlogCard
+                    id={blog.id}
+                    authorName={blog.author.name || "Anonymous"}
+                    title={blog.title}
+                    content={blog.content}
+                    publishedDate={"2nd Feb 2024"}
+                />)}
+            </div>
+        </div>
+    </div>
+}
+///////////////////////////
